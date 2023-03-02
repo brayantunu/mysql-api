276787b07b339379b5f323df0b5eae8b85ec87ec
brayantunu <bstunubala@misena.edu.co>
2023-03-02 17:36:44 -0500

    hola2

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import  Express  from "express";
+import employes from "./router/employes-router.js"
+import index from "./router/index-router.js"
+const app = Express()
+
+app.use(Express.json())
+app.use('/api',employes)
+app.use(index)
+
+app.use((req,res,next) => {
+    res.status(400).json({message: 'not found'
+    })
+})
+// 
+
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-import  Express  from "express";
-import employes from "./router/employes-router.js"
-import index from "./router/index-router.js"
-const app = Express()
 
-app.use(Express.json())
-app.listen(3000)
-app.use('/api',employes)
-app.use(index)
-console.log("serve corriendo",3000);
+import app from './app.js';
+import {PORT} from './config.js'
+app.listen(PORT)
+console.log("serve corriendo",PORT);
