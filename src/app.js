b6b62a538832628236d3111ed76f18174a639f6b
brayantunu <bstunubala@misena.edu.co>
2023-03-07 20:46:14 -0500

    solucionado el pong con la conecion de la base de datos mysql

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use((req,res,next) => {
     res.status(400).json({message: 'not found'
     })
 })
-// 
 
 
-export default app;
+
+export default app; 
--- src/db.js
+++ src/db.js
@@ -1,4 +1,5 @@
 import { createPool } from "mysql2/promise";
+// const mysql = require('mysql');
 
 import {
     DB_DATABASE,
@@ -8,10 +9,25 @@ import {
     DB_USER
 }
 from './config.js'
-export const pool =  createPool({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    port: DB_PORT,
-    database: DB_DATABASE
-})
+// export const pool =  createPool({
+//     host: DB_HOST,
+//     user: DB_USER,
+//     password: DB_PASSWORD,
+//     port: DB_PORT,
+//     database: DB_DATABASE
+// })
+
+export const pool = createPool({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE
+});
+
+pool.getConnection((error, connection) => {
+  if (error) {
+    console.error('Error de conexión: ' + error.stack);
+    return;
+  }
+  console.log('Conexión establecida con el ID: ' + connection.threadId);
+});
