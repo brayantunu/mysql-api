64462e18a8028db8a7be9a51f916513023a048c1
brayantunu <bstunubala@misena.edu.co>
2023-03-11 17:06:53 -0500

    yuliana2

--- src/router/employes-router.js
+++ src/router/employes-router.js
@@ -2,8 +2,8 @@ import { Router } from "express";
 import { createempoyes, deleteEmpoyes, getempoyes, getempoyesid, updateEmpoyes } from "../controller/employer-controller.js";
 const router = Router()
 
-router.get('/employes',getempoyes);
-router.post('/employes',createempoyes);
+router.get('/getemployes',getempoyes);
+router.post('/createemployes',createempoyes);
 router.put('/employes/:id',updateEmpoyes );
 router.delete('/employes/:id',deleteEmpoyes );
 router.get('/employes/:id',getempoyesid);
